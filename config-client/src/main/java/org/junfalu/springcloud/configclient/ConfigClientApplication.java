a03b7fc675f39309681d74a0d9e6a33359b9d25e
lujunfa <2990606482@qq.com>
2018-12-04 16:05:16 +0800

    基于消息总线的动态配置信息刷新

--- config-client/src/main/java/org/junfalu/springcloud/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/org/junfalu/springcloud/configclient/ConfigClientApplication.java
@@ -1,5 +1,7 @@
 package org.junfalu.springcloud.configclient;
 
+import org.junfalu.springcloud.configclient.RandomServerPort.MyApplicationEnvironmentPreparedEventListener;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,6 +11,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
-       new SpringApplicationBuilder(ConfigClientApplication.class).web(true).run(args);
+        SpringApplication app = new SpringApplication(ConfigClientApplication.class);
+        app.addListeners(new MyApplicationEnvironmentPreparedEventListener());
+        app.run(args);
     }
 }
